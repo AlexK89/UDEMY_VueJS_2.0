a711e6df8f8a74bf3e12e125f03bf5188d73efab
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-04-04 21:44:56 +0100

    hello world

--- hello_world/src/main.js
+++ hello_world/src/main.js
@@ -1,11 +1,15 @@
 new Vue({
 	el: '#vue-section',
 	data: {
-		title: 'Type any text inside the field'
+		title: 'Type any text inside the field',
+		link: 'google.com'
 	},
 	methods: {
 		changeTitle: function (event) {
 			this.title = event.target.value;
+		},
+		sayHello: function () {
+			return 'hello:)'
 		}
 	}
 });

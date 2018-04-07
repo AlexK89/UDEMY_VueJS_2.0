0a7231c14d76e084fc8bcc3c9fa3e9026eaf1e0b
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-04-07 20:53:24 +0100

    watch - execute code upon data changes

--- events/src/main.js
+++ events/src/main.js
@@ -38,3 +38,55 @@ new Vue({
 		}
 	}
 });
+
+new Vue({
+	el: '#app_2',
+	data: {
+		counter: 0,
+		result: ''
+	},
+	methods: {
+		increaseCounter: function () {
+			this.counter++;
+			this.getResult();
+		},
+		decreaseCounter: function () {
+			this.counter--;
+			this.getResult();
+		},
+		getResult: function () {
+			this.result = this.counter > 5 ? 'greater then 5' : 'less then 5';
+		}
+	}
+});
+
+new Vue({
+	el: '#app_3',
+	data: {
+		counter: 0,
+		counter_2: 0,
+		result: ''
+	},
+	//computed works just synchronously
+	computed: {
+		output() {
+			return this.result = this.counter > 5 ? ('greater then 5: ' + Math.random()) : ('less then 5: ' + Math.random());
+		}
+	},
+	// watch execute code upon data changes
+	// if you need async for 'computed' you need to use 'watch'
+	watch: {
+		// property needs to be the same as above to make it change
+		counter(value) {
+			let vueObj = this;
+			setTimeout(()=>{
+				vueObj.counter = 0;
+			},5000);
+		}
+	},
+	methods: {
+		increaseCounter: function () {
+			return this.result = this.counter > 5 ? ('greater then 5: ' + Math.random()) : ('less then 5: ' + Math.random());
+		},
+	}
+});

628a279a3dd90a0e2ad3fe826deb73633365ee4b
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-04-08 15:58:36 +0100

    exercise 4

--- dynamic_styling_css/src/main.js
+++ dynamic_styling_css/src/main.js
@@ -6,7 +6,7 @@ new Vue({
 		width: null,
 		height: null
 	},
-	computed:{
+	computed: {
 		divColor() {
 			return {
 				'green': this.attachGreen,
@@ -19,6 +19,53 @@ new Vue({
 				height: this.height + 'px'
 			}
 
+		}
+	}
+});
+
+new Vue({
+	el: '#exercise',
+	data: {
+		styles: '',
+		colorRed: 'red',
+		border: 'box-border',
+		boxStyle: '',
+		extraClass: false,
+		inputClass: '',
+		test: 100,
+		myStyle: {
+			background: 'green'
+		},
+		progressBar: {
+			height: '20px',
+			background: 'red',
+			width: 0,
+			transition: '5s'
+		}
+	},
+	methods: {
+		startEffect() {
+			return setInterval(() => {
+				if (this.styles === 'highlight') {
+					this.styles = 'shrink';
+				} else {
+					this.styles = 'highlight';
+				}
+			}, 3000);
+		},
+		startProgress() {
+			let vm = this;
+			let width = 0;
+
+			let interval = setInterval(() => {
+				console.log(width);
+				if (width < 1000) {
+					width += 30;
+					vm.progressBar.width = width + 'px';
+				} else {
+					clearInterval(interval);
+				}
+			}, 100)
 		}
 	}
 });

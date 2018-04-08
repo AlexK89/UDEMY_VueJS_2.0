ce13681301bfe9b9cbf2d2143897367eac3b4763
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-04-08 10:26:30 +0100

    styling with VueJS:Dynamic Styling with CSS Classes - Using Objects

--- dynamic_styling_css/src/main.js
+++ dynamic_styling_css/src/main.js
@@ -2,5 +2,13 @@ new Vue({
 	el: '#vue-styling',
 	data: {
 		attachGreen: false
+	},
+	computed:{
+		divColor() {
+			return {
+				'green': this.attachGreen,
+				'blue': !this.attachGreen
+			}
+		}
 	}
 });

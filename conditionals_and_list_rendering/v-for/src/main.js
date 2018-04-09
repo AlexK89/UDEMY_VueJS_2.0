aead2310bacfb4562c2e14db390f8f76ce528370
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-04-09 19:51:27 +0100

    v-for: looping through arrays and objects

---
+++ conditionals_and_list_rendering/v-for/src/main.js
@@ -0,0 +1,10 @@
+new Vue({
+	el: '#app',
+	data: {
+		ingredients: ['bananas', 'strawberries', 'yogurt'],
+		persons: [
+			{name: 'Alex', age: '20', color: 'green'},
+			{name: 'Kristina', age: '21', color: 'pink'}
+		]
+	}
+});

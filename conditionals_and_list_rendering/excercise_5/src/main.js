2a92dd5cb4bd88cbc7f2d82c570a6a81708ed0c4
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-04-09 20:55:38 +0100

    excercise 5

---
+++ conditionals_and_list_rendering/excercise_5/src/main.js
@@ -0,0 +1,16 @@
+new Vue({
+	el: '#app',
+	data: {
+		show: true,
+		fishes: ['cod', 'salmon', 'fish'],
+		book: {
+			title: 'Lord of the Rings',
+			author: 'J.R.R. Tolkiens',
+			books: '3'
+		},
+		object: {
+			name: 'TESTOBJECT',
+			data: [1.67, 1.33, 0.98, 2.21]
+		}
+	}
+});

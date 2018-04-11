2dd4fedd4184fbb2dcbb60254e118cf19e951a8a
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-04-11 20:50:41 +0100

    monster_slayer - turns logs

--- monster_slayer/src/app.js
+++ monster_slayer/src/app.js
@@ -3,7 +3,8 @@ new Vue({
 	data: {
 		playerHealth: 100,
 		monsterHealth: 100,
-		gameRunning: false
+		gameRunning: false,
+		turns: []
 	},
 	methods: {
 		// Main functionality
@@ -11,37 +12,71 @@ new Vue({
 			this.gameRunning = true;
 			this.playerHealth = 100;
 			this.monsterHealth = 100;
+			this.turns = [];
 		},
 		defaultAttack() {
 			// passing 4 params: playerMin, playerMax, monsterMin, monsterMax
-			this.attack(3, 10, 0, 12);
+			this.attack(3, 10, 3, 15);
 		},
 
 		specialAttack() {
 			// passing 4 params: playerMin, playerMax, monsterMin, monsterMax
-			this.attack(8, 16, 0, 20);
+			this.attack(5, 16, 5, 22);
 		},
 		heal() {
-			this.playerHealth += this.damageMath(6, 10);
-			this.monsterAttack(0, 12);
+			this.monsterAttack(0, 15);
+
+			// For "damageMath" pass parameters: minDamage, maxDamage
+			let healedBy = this.damageMath(6, 10);
+
+			this.playerHealth += healedBy;
+			if (this.playerHealth > 100) {
+				this.playerHealth = 100;
+			}
+
+			this.turns.unshift({
+				isPlayer: true,
+				text: `You healed by ${healedBy}`
+			});
 		},
 		resetGame() {
-
+			this.playerHealth = 0;
+			setTimeout(() => {
+				alert('You have been defeated');
+			}, 600);
+			this.gameRunning = false
 		},
 
 		//supplemental methods
 		attack(playerMin, playerMax, monsterMin, monsterMax) {
-			this.monsterHealth -= this.damageMath(playerMin, playerMax);
+			// For "damageMath" pass parameters: minDamage, maxDamage
+			let damage = this.damageMath(playerMin, playerMax);
+
+			this.monsterHealth -= damage;
+
+			//Pass parameters: attacker, target, damage, isPlayer
+			this.commitTurn('Player', 'Monster', damage, true);
 			if (this.checkResult()) {
 				return;
 			}
 
-			this.monsterAttack(playerMin, playerMax);
+			this.monsterAttack(monsterMin, monsterMax);
 			this.checkResult();
 		},
 		// we use separate method to make it accessible for healing step
 		monsterAttack(monsterMin, monsterMax) {
-			this.playerHealth -= this.damageMath(monsterMin, monsterMax);
+			let damage = this.damageMath(monsterMin, monsterMax);
+
+			this.playerHealth -= damage;
+
+			//Pass parameters: attacker, target, damage, isPlayer
+			this.commitTurn('Monster', 'Player', damage, false);
+		},
+		commitTurn(attacker, target, damage, isPlayer) {
+			this.turns.unshift({
+				isPlayer: isPlayer,
+				text: `${attacker} hits ${target} by ${damage}`
+			});
 		},
 		damageMath(minDamage, maxDamage) {
 			return Math.max(Math.ceil(Math.random() * maxDamage), minDamage);

2b1fc2da3ab3fa101f440d78866f809949be6a7f
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-04-11 18:44:48 +0100

    monster_slayer - attack math and check result method

--- monster_slayer/src/app.js
+++ monster_slayer/src/app.js
@@ -10,6 +10,37 @@ new Vue({
 			this.gameRunning = true;
 			this.playerHealth = 100;
 			this.monsterHealth = 100;
-		}
+		},
+		attack() {
+			this.monsterHealth -= this.damageMath(3, 10);
+			this.playerHealth -= this.damageMath(0, 12);
+
+			this.checkResult();
+		},
+		checkResult() {
+			if (this.monsterHealth <= 0) {
+				this.monsterHealth = 0;
+				this.gameRunning = false;
+				
+				alert("you win");
+			} else if (this.playerHealth <= 0) {
+				this.playerHealth = 0;
+				this.gameRunning = false;
+
+				alert("you lose");
+			}
+		},
+		damageMath(minDamage, maxDamage) {
+			return Math.max(Math.ceil(Math.random() * maxDamage), minDamage);
+		},
+		specialAttack() {
+
+		},
+		heal() {
+
+		},
+		resetGame() {
+
+		},
 	}
 });

8e22abaa15069973c74c7b41046c83a597ead2a1
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-04-12 20:19:45 +0100

    vuejs instances

--- instances/src/main.js
+++ instances/src/main.js
@@ -1,9 +1,16 @@
 new Vue({
 	el: '#app',
 	data: {
-
+		title: 'vuejs',
+		showParagraph: false
 	},
 	methods: {
-
+		show() {
+			this.showParagraph = true;
+			this.updateTitle('The vuejs instance');
+		},
+		updateTitle(title) {
+			this.title = title;
+		}
 	}
 });

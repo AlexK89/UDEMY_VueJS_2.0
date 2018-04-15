9c0b4734afc53abb5b5bd0cca7c3025ac8dd2728
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-04-15 13:44:41 +0100

    vue templates excercise:solution

---
+++ vue-cli-excercise/src/Servers.vue
@@ -0,0 +1,22 @@
+<template>
+	<ul class="list-group">
+		<li class="list-group-item" v-for="index in serversNumber">
+			Server #{{ index }}
+		</li>
+	</ul>
+</template>
+
+<script>
+	export default {
+		name: "Servers",
+		data() {
+			return {
+				serversNumber: [1, 2, 3, 4, 5]
+			}
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>

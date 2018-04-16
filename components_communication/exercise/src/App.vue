3789da9aafa4f81c5a7eb8c9dab641d5208819d9
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-04-16 21:39:48 +0100

    vue components comunication: excercise v.0.2

--- components_communication/exercise/src/App.vue
+++ components_communication/exercise/src/App.vue
@@ -1,30 +1,40 @@
 <template>
-    <div class="container">
-        <app-header></app-header>
-        <hr>
-        <div class="row">
-            <servers></servers>
-            <app-server-details></app-server-details>
-        </div>
-        <hr>
-        <app-footer></app-footer>
-    </div>
+	<div class="container">
+		<app-header></app-header>
+		<hr>
+		<div class="row">
+			<servers :servers="servers"></servers>
+			<app-server-details></app-server-details>
+		</div>
+		<hr>
+		<app-footer></app-footer>
+	</div>
 </template>
 
 <script>
-    import Header from './components/Shared/Header.vue';
-    import Footer from './components/Shared/Footer.vue';
-    import Servers from './components/Server/Servers.vue';
-    import ServerDetails from './components/Server/ServerDetails.vue';
+	import Header from './components/Shared/Header.vue';
+	import Footer from './components/Shared/Footer.vue';
+	import Servers from './components/Server/Servers.vue';
+	import ServerDetails from './components/Server/ServerDetails.vue';
 
-    export default {
-        components: {
-            appHeader: Header,
-            Servers,
-            'app-server-details': ServerDetails,
-            'app-footer': Footer
-        }
-    }
+	export default {
+		data() {
+			return {
+				servers: [
+					{id: '001', name: 'Mac', status: 'good'},
+					{id: '002', name: 'Linux', status: 'poor'},
+					{id: '003', name: 'Debian', status: 'good'},
+					{id: '004', name: 'Windows', status: 'critical'}
+				]
+			}
+		},
+		components: {
+			appHeader: Header,
+			Servers,
+			'app-server-details': ServerDetails,
+			'app-footer': Footer
+		}
+	}
 </script>
 
 <style>
--- components_communication/exercise/src/components/Server/Servers.vue
+++ components_communication/exercise/src/components/Server/Servers.vue
@@ -1,18 +1,17 @@
 <template>
-    <div class="col-xs-12 col-sm-6">
-        <ul class="list-group">
-            <li
-                    class="list-group-item"
-                    v-for="index in 5">
-                Server #{{ index }}
-            </li>
-        </ul>
-    </div>
+	<div class="col-xs-12 col-sm-6">
+		<ul class="list-group">
+			<li
+					class="list-group-item"
+					v-for="server in servers">
+				Server #{{ server.id }}; Name: {{ server.name }}; Status: {{ server.status }}
+			</li>
+		</ul>
+	</div>
 </template>
 
 <script>
+	export default {
+		props: ['servers'],
+	}
 </script>
-
-<style>
-
-</style>

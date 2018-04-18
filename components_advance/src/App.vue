94aa6cd8624341d6f1d03d0dddabe40a7402f753
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-04-18 21:21:32 +0100

    components advance: quote

--- components_advance/src/App.vue
+++ components_advance/src/App.vue
@@ -1,18 +1,21 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-               
-            </div>
-        </div>
-    </div>
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<our-quote></our-quote>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
+	import Quote from './components/Quote.vue'
 
-    export default {
-        
-    }
+	export default {
+		components: {
+			ourQuote: Quote
+		}
+	}
 </script>
 
 <style>
---
+++ components_advance/src/components/Quote.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="quote">
+		<p>Our quote</p>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "Quote"
+	}
+</script>
+
+<style scoped>
+	.quote {
+		background-color: #5c6bc0;
+		color: #ffffff;
+		text-align: center;
+		margin: 2em auto;
+		padding: 30px;
+		box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19),
+					0 6px 6px rgba(0, 0, 0, 0.23);
+	}
+</style>

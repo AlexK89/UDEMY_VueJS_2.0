bb6fd24724f60a63aaf4ec438b2c22f20981a489
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-04-19 20:13:26 +0100

    components advance: dynamic components 'component' tag

--- components_advance/src/App.vue
+++ components_advance/src/App.vue
@@ -2,10 +2,17 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-12">
-				<our-quote>
-					<h3 slot="title">Quote title</h3>
-					<p slot="body">our quote body</p>
-				</our-quote>
+				<button @click="selectedComponent='ourQuote'">Quote</button>
+				<button @click="selectedComponent='ourAuthor'">Author</button>
+				<button @click="selectedComponent='ourNew'">New</button>
+				<!--Dynamic components-->
+				<component :is="selectedComponent">
+					<p>Default Content</p>
+				</component>
+				<!--<our-quote>-->
+					<!--<h3 slot="title">Quote title</h3>-->
+					<!--<p slot="body">our quote body</p>-->
+				<!--</our-quote>-->
 			</div>
 		</div>
 	</div>
@@ -13,10 +20,20 @@
 
 <script>
 	import Quote from './components/Quote.vue'
+	import Author from './components/Author.vue'
+	import New from './components/New.vue'
 
 	export default {
+		data() {
+			return {
+				quoteTitle: 'The Quote',
+				selectedComponent: 'ourQuote',
+			}
+		},
 		components: {
-			ourQuote: Quote
+			ourQuote: Quote,
+			ourAuthor: Author,
+			ourNew: New,
 		}
 	}
 </script>

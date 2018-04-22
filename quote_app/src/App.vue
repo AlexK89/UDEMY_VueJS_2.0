2e34c4499c93588cb3a31494a163c52d65a920a0
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-04-22 11:52:04 +0100

    vue_quote_app: v.0.1.4

--- quote_app/src/App.vue
+++ quote_app/src/App.vue
@@ -1,16 +1,23 @@
 <template>
 	<div class="container">
-
+		<app-quote-grid :quotes="quotes"></app-quote-grid>
 	</div>
 </template>
 
 <script>
+	import QuoteGrid from './components/QuoteGrid.vue';
+
 	export default {
 		data() {
 			return {
-				quotes: [],
+				quotes: [
+					'test quote'
+				],
 				maxQuotes: 10
 			}
+		},
+		components: {
+			appQuoteGrid: QuoteGrid
 		}
 	}
 </script>
---
+++ quote_app/src/components/Quote.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="col-sm-6 col-md-3">
+		<div class="panel">
+			<div class="panel-body quote">
+				<slot></slot>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "Quote"
+	}
+</script>
+
+<style scoped>
+	.quote {
+		background-color: #d5d5d5;
+	}
+
+	.quote:hover {
+		 background-color: #00bfa5;
+	 }
+</style>

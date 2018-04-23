819e0e63521ee9d33610a335a35ca6dad9f5ce96
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-04-23 10:57:21 +0100

    vue_forms_handling: v-model for checkboxes

--- vue_handling_forms/src/App.vue
+++ vue_handling_forms/src/App.vue
@@ -52,13 +52,15 @@
 							<input
 									type="checkbox"
 									id="sendmail"
-									value="SendMail"> Send Mail
+									value="SendMail"
+									v-model="sendMail"> Send Mail
 						</label>
 						<label for="sendInfomail">
 							<input
 									type="checkbox"
 									id="sendInfomail"
-									value="SendInfoMail"> Send Infomail
+									value="SendInfoMail"
+									v-model="sendMail"> Send Infomail
 						</label>
 					</div>
 
@@ -114,7 +116,9 @@
 						<p style="white-space: pre;">Message: {{ message }}</p>
 						<p><strong>Send Mail?</strong></p>
 						<ul>
-							<li></li>
+							<li v-for="item in sendMail">
+								{{ item }}
+							</li>
 						</ul>
 						<p>Gender:</p>
 						<p>Priority:</p>
@@ -135,7 +139,8 @@
 					password: '',
 					age: '',
 				},
-				message: 'Default text'
+				message: 'Default text',
+				sendMail: []
 			}
 		}
 	}

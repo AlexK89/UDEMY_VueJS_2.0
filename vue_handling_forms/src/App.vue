a4d9d42c14ccc0ad39c3d84910b0417afebcb66a
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-04-23 11:14:12 +0100

    vue_forms_handling: v-model for select options

--- vue_handling_forms/src/App.vue
+++ vue_handling_forms/src/App.vue
@@ -87,10 +87,13 @@
 			<div class="row">
 				<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group">
 					<label for="priority">Priority</label>
+					<!--we use v-model for whole select element, not option-->
 					<select
 							id="priority"
-							class="form-control">
-						<option></option>
+							class="form-control" v-model="priorities.prioritySelected">
+						<option v-for="priority in priorities.priorityOptions">
+							{{ priority }}
+						</option>
 					</select>
 				</div>
 			</div>
@@ -123,7 +126,7 @@
 							</li>
 						</ul>
 						<p>Gender: {{ userData.gender }}</p>
-						<p>Priority:</p>
+						<p>Priority: {{ priorities.prioritySelected }}</p>
 						<p>Switched:</p>
 					</div>
 				</div>
@@ -144,6 +147,10 @@
 				},
 				message: 'Default text',
 				sendMail: [],
+				priorities: {
+					priorityOptions: ['High', 'Normal', 'Low'],
+					prioritySelected: 'Low'
+				}
 			}
 		}
 	}

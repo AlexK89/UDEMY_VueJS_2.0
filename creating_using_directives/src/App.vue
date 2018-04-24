0cafecfc17833bfeb9f70baf7097d8e03d31a34f
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-04-24 20:27:29 +0100

    [creating&using directives] - custom directive with parameter

--- creating_using_directives/src/App.vue
+++ creating_using_directives/src/App.vue
@@ -1,3 +1,5 @@
+
+
 <template>
 	<div class="container">
 		<div class="row">
@@ -10,7 +12,7 @@
 
 		<div class="row">
 			<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-				<h1>Custom Directives</h1>
+				<h1 v-highlight="'grey'">Custom Directives</h1>
 				<p v-text="'v-text - Hello World'"></p>
 				<p v-html="'v-html - <strong>html text</strong>'"></p>
 			</div>
--- creating_using_directives/src/main.js
+++ creating_using_directives/src/main.js
@@ -1,9 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// Directives have 5 HOOKs:
+// - bind(el, binding, vnode) - once directive is attached
+// - inserted(el, binding, vnode) - inserted in parentNode
+// - update(el, binding, vnode, oldVnode) - once component is updated(without children)
+// - componentUpdated(el, binding, vnode, oldVnode) - once component is updated(with children)
+// - unbind(el, binding, vnode) - once directive is removed
+
 //custom directives
 Vue.directive('highlight', {
-	
+	bind(el, binding, vnode) {
+		// el.style.background = 'green';
+		el.style.background = binding.value;
+	}
 });
 
 new Vue({

820b9f1815fcde8c9e6d7615c7133daec2870ec9
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-04-24 19:14:55 +0100

    [creating&using directives] - basic setup

---
+++ creating_using_directives/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+				<h1>Build in Directives</h1>
+				<p v-text="'v-text - Hello World'"></p>
+				<p v-html="'v-html - <strong>html text</strong>'"></p>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+				<h1>Custom Directives</h1>
+				<p v-text="'v-text - Hello World'"></p>
+				<p v-html="'v-html - <strong>html text</strong>'"></p>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {}
+</script>
+
+<style>
+
+</style>

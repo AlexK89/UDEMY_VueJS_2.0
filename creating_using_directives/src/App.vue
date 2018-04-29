93d12593faa2dcf2e9aab96cbbefb469caec3b49
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-04-29 12:31:19 +0100

    [creating&using directives] - multiple modifiers

--- creating_using_directives/src/App.vue
+++ creating_using_directives/src/App.vue
@@ -14,7 +14,7 @@
 				<!--delayed is a modifier, we can create and use a lot of modifiers-->
 				<h1 v-highlight:background.delayed="'green'">Custom Directives</h1>
 				<h1 v-highlight="'green'">Custom Directives</h1>
-				<h1 v-local-highlight:background.delayed="'green'">Custom Directives</h1>
+				<h1 v-local-highlight:background.delayed.blink="'green'">Custom Directives</h1>
 				<p v-text="'v-text - Hello World'"></p>
 				<p v-html="'v-html - <strong>html text</strong>'"></p>
 			</div>
@@ -28,17 +28,38 @@
 			'localHighlight': {
 				bind(el, binding, vnode) {
 					let delay = 0;
+
 					if (binding.modifiers['delayed']) {
 						delay = 3000;
 					}
 
-					setTimeout(() => {
-						if (binding.arg == 'background') {
-							el.style.background = binding.value;
-						} else {
-							el.style.color = binding.value;
-						}
-					}, delay);
+					if (binding.modifiers['blink']) {
+						let mainColor = binding.value;
+						let secondColor = 'blue';
+						let currentColor = mainColor;
+
+						setTimeout(() => {
+							setInterval(() => {
+								currentColor === mainColor ? currentColor = secondColor : currentColor = mainColor;
+
+								if (binding.arg == 'background') {
+									el.style.background = currentColor;
+								} else {
+									el.style.color = currentColor;
+								}
+							}, 1000);
+
+						}, 3000);
+
+					} else {
+						setTimeout(() => {
+							if (binding.arg == 'background') {
+								el.style.background = binding.value;
+							} else {
+								el.style.color = binding.value;
+							}
+						}, delay);
+					}
 				}
 			}
 		}

ab36868a1fdadf26cfb5d9d43e318bd7b785e253
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-04-29 11:43:06 +0100

    [creating&using directives] - modifiers

--- creating_using_directives/src/App.vue
+++ creating_using_directives/src/App.vue
@@ -11,7 +11,8 @@
 		</div>
 		<div class="row">
 			<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-				<h1 v-highlight:background="'green'">Custom Directives</h1>
+				<!--delayed is a modifier, we can create and use a lot of modifiers-->
+				<h1 v-highlight:background.delayed="'green'">Custom Directives</h1>
 				<h1 v-highlight="'green'">Custom Directives</h1>
 				<p v-text="'v-text - Hello World'"></p>
 				<p v-html="'v-html - <strong>html text</strong>'"></p>
--- creating_using_directives/src/main.js
+++ creating_using_directives/src/main.js
@@ -12,11 +12,20 @@ import App from './App.vue'
 Vue.directive('highlight', {
 	bind(el, binding, vnode) {
 		// el.style.background = 'green';
-		if(binding.arg == 'background') {
-			el.style.background = binding.value;
-		} else {
-			el.style.color = binding.value;
+
+		let delay = 0;
+		if (binding.modifiers['delayed']) {
+			delay = 3000;
 		}
+
+		setTimeout(() => {
+			if (binding.arg == 'background') {
+				el.style.background = binding.value;
+			} else {
+				el.style.color = binding.value;
+			}
+		}, delay);
+
 	}
 });
 

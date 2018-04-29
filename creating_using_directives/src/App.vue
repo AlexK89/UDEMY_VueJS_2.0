6966f54ffbd2e2ba0ca177acc45f044626233c7e
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-04-29 12:20:25 +0100

    [creating&using directives] - local directives

--- creating_using_directives/src/App.vue
+++ creating_using_directives/src/App.vue
@@ -14,6 +14,7 @@
 				<!--delayed is a modifier, we can create and use a lot of modifiers-->
 				<h1 v-highlight:background.delayed="'green'">Custom Directives</h1>
 				<h1 v-highlight="'green'">Custom Directives</h1>
+				<h1 v-local-highlight:background.delayed="'green'">Custom Directives</h1>
 				<p v-text="'v-text - Hello World'"></p>
 				<p v-html="'v-html - <strong>html text</strong>'"></p>
 			</div>
@@ -22,7 +23,25 @@
 </template>
 <script>
 	export default {
-		name: 'App'
+		name: 'App',
+		directives: {
+			'localHighlight': {
+				bind(el, binding, vnode) {
+					let delay = 0;
+					if (binding.modifiers['delayed']) {
+						delay = 3000;
+					}
+
+					setTimeout(() => {
+						if (binding.arg == 'background') {
+							el.style.background = binding.value;
+						} else {
+							el.style.color = binding.value;
+						}
+					}, delay);
+				}
+			}
+		}
 	}
 </script>
 

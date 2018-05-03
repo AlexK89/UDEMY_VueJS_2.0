0913cc89b536262fc44d5ad44f3d4c3925bfbffa
Kozhushko Alexandr <kozhushkoalexandr@Kozhushkos-MacBook-Pro.local>
2018-05-03 23:44:48 +0100

    folder update

--- filters_mixins/src/List.vue
+++ filters_mixins/src/List.vue
@@ -3,9 +3,6 @@
 		<div class="row">
 			<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
 				<h1>Filters & Mixins</h1>
-				<p>{{ text | toUppercase | toLowercase }}</p>
-				<hr>
-				<input type="text" v-model="filteredText">
 				<ul>
 					<li v-for="fruit in filteredFruits">{{ fruit }}</li>
 				</ul>
@@ -18,15 +15,8 @@
 	export default {
 		data() {
 			return {
-				text: 'Hello world',
 				fruits: ['apple', 'banana', 'orange', 'watermelon'],
 				filteredText: ''
-			}
-		},
-		filters: {
-			// 'value' - is must have, means value we want to transform
-			toUppercase(value) {
-				return value.toUpperCase();
 			}
 		},
 		computed: {

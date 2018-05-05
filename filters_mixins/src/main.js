ec2e820c61d362be187760d53fe3819c2c80fd06
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-05-05 21:08:39 +0100

    [filters_mixins] - global mixins

--- filters_mixins/src/main.js
+++ filters_mixins/src/main.js
@@ -5,6 +5,15 @@ Vue.filter('toLowercase', (value) => {
 	return value.toLowerCase();
 });
 
+//global mixin
+
+Vue.mixin({
+	created() {
+		console.log('Global MIXIN - Created Hook')
+	}
+});
+
+//the third time of calling a Global mixin is because this Vue object
 new Vue({
 	el: '#app',
 	render: h => h(App)

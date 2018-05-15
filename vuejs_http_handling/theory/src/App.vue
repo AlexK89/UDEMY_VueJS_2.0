9755fa6af04712e5e8d12fa3e64f5e6c138be24c
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-05-15 22:01:28 +0100

    [vue_http_handling] - get data from Server

--- vuejs_http_handling/theory/src/App.vue
+++ vuejs_http_handling/theory/src/App.vue
@@ -12,6 +12,12 @@
 					<input type="email" class="form-control" id="email" v-model="user.userEmail">
 				</div>
 				<button class="btn btn-primary" @click="submitForm">Submit</button>
+				<hr>
+				<button class="btn btn-primary" @click="fetchData">Get data</button>
+				<br><br>
+				<ul class="list-group">
+					<li class="list-group-item" v-for="user in users">Name: {{ user.userName }}, Email: {{user.userEmail}}</li>
+				</ul>
 			</div>
 		</div>
 	</div>
@@ -24,7 +30,8 @@
 				user: {
 					userName: '',
 					userEmail: ''
-				}
+				},
+				users: []
 			}
 		},
 		methods: {
@@ -34,6 +41,20 @@
 						console.log(response);
 					}, error => {
 						console.log(error);
+					})
+			},
+			fetchData() {
+				this.$http.get('https://vuejs-udemy-ca814.firebaseio.com/users.json')
+					.then(response => {
+						return response.json();
+					})
+					.then(data => {
+						const usersDB = [];
+
+						for (let key in data) {
+							usersDB.push(data[key]);
+						}
+						this.users = usersDB;
 					})
 			}
 		}

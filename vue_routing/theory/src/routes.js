142f30a8f7a45b261697bd1fca725567f8ca47ee
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-05-19 20:19:53 +0100

    [vuejs_routing] - multiple router views

--- vue_routing/theory/src/routes.js
+++ vue_routing/theory/src/routes.js
@@ -1,17 +1,30 @@
 import Home from './components/Home.vue';
+import Header from './components/Header.vue';
 import User from './components/user/User.vue';
 import UserStart from './components/user/UserStart.vue';
 import UserDetail from './components/user/UserDetail.vue';
 import UserEdit from './components/user/UserEdit.vue';
 
 export const routes = [
-	{path: '', component: Home, name: 'home'},
+	{
+		path: '', name: 'home', components: {
+			default: Home,
+			'header-top': Header
+		}
+	},
 	// :id makes us pass dynamic data
 	//we use parameter children to pass for the components own routes
-	{path: '/user/', component: User, children: [
-			{ path: '', component: UserStart },
-			{ path: ':id', component: UserDetail },
+	{
+		path: '/user/', children: [
+			{path: '', component: UserStart},
+			{path: ':id', component: UserDetail},
 			//we can edit name property to make your code cleaner
-			{ path: ':id/edit', component: UserEdit, name: 'userEdit' },
-		]}
+			{path: ':id/edit', component: UserEdit, name: 'userEdit'},
+		],
+		components: {
+			default: User,
+			'header-top': Header,
+			'header-bottom': Header
+		}
+	}
 ];

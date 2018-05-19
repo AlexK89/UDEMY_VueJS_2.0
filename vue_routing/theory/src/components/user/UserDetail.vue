0785e546e8b5ef4c47283d6ad612c4204e885e4d
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-05-19 19:55:52 +0100

    [vuejs_routing] - query params

--- vue_routing/theory/src/components/user/UserDetail.vue
+++ vue_routing/theory/src/components/user/UserDetail.vue
@@ -2,6 +2,8 @@
 	<div>
 		<h3>Some User Details</h3>
 		<p>User id: {{ $route.params.id }}</p>
-		<router-link tag="button" :to="{name: 'userEdit', params: { id: $route.params.id }}" class="btn btn-danger">Edit</router-link>
+		<router-link tag="button"
+					 :to="{name: 'userEdit', params: { id: $route.params.id }, query: {locale: 'en', q: 100}}"
+					 class="btn btn-danger">Edit</router-link>
 	</div>
 </template>
--- vue_routing/theory/src/components/user/UserEdit.vue
+++ vue_routing/theory/src/components/user/UserEdit.vue
@@ -1,3 +1,8 @@
 <template>
-    <h3>Edit the User</h3>
+	<div>
+		<h3>Edit the User</h3>
+		<hr>
+		<p>Number: {{$route.query.q}}</p>
+		<p>Language: {{$route.query.locale}}</p>
+	</div>
 </template>

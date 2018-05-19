6bb5cee91515e721c1f5c34b203f7ae99af8651f
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-05-19 10:19:52 +0100

    [vuejs_routing] - navigation from code

--- vue_routing/theory/src/components/user/User.vue
+++ vue_routing/theory/src/components/user/User.vue
@@ -1,4 +1,15 @@
 <template>
-    <h1>The User Page</h1>
-
+	<div>
+		<h1>The User Page</h1>
+		<button class="btn btn-default" @click="navigateHome">Go to Home</button>
+	</div>
 </template>
+<script>
+	export default {
+		methods: {
+			navigateHome() {
+				this.$router.push('/')
+			}
+		}
+	}
+</script>

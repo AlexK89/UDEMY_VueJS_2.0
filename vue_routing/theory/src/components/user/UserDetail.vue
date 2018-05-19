684fdda75778f8f7035ce12310cdeb44a0cd45dd
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-05-19 21:02:50 +0100

    [vuejs_routing] - passing hash fragment

--- vue_routing/theory/src/components/user/UserDetail.vue
+++ vue_routing/theory/src/components/user/UserDetail.vue
@@ -3,7 +3,28 @@
 		<h3>Some User Details</h3>
 		<p>User id: {{ $route.params.id }}</p>
 		<router-link tag="button"
-					 :to="{name: 'userEdit', params: { id: $route.params.id }, query: {locale: 'en', q: 100}}"
-					 class="btn btn-danger">Edit</router-link>
+					 :to="link"
+					 class="btn btn-danger">Edit
+		</router-link>
 	</div>
 </template>
+<script>
+	export default {
+		data() {
+			return {
+				link: {
+					name: 'userEdit',
+					params: {
+						id: this.$route.params.id
+					},
+					query: {
+						locale: 'en',
+						q: 100
+					},
+					//to jump directly to element with id
+					hash: '#data'
+				}
+			}
+		}
+	}
+</script>
--- vue_routing/theory/src/components/user/UserEdit.vue
+++ vue_routing/theory/src/components/user/UserEdit.vue
@@ -4,5 +4,8 @@
 		<hr>
 		<p>Number: {{$route.query.q}}</p>
 		<p>Language: {{$route.query.locale}}</p>
+		<hr>
+		<div style="height: 1000px; background-color: cornflowerblue"></div>
+		<p id="data">Some data</p>
 	</div>
 </template>

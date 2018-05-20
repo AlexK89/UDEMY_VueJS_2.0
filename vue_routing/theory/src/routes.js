4c2b89f5a1f3d34583ddb8c06c67231199289d49
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-05-20 10:35:26 +0100

    [vuejs_routing] - lazy loading for routes | grouping components

--- vue_routing/theory/src/routes.js
+++ vue_routing/theory/src/routes.js
@@ -2,16 +2,17 @@ import Home from './components/Home.vue';
 import Header from './components/Header.vue';
 
 // LAZY LOADER - loads when it requires !!! VERY USEFUL FOR BIG APPS
+// we can group their loading by adding extra param "name". For ex: user
 const User = resolve => {
 	require.ensure(['./components/user/User.vue'], () => {
 		resolve(require('./components/user/User.vue'));
-	});
+	}, 'user');
 };
 
 const UserStart = resolve => {
 	require.ensure(['./components/user/UserStart.vue'], () => {
 		resolve(require('./components/user/UserStart.vue'));
-	});
+	}, 'user');
 };
 
 const UserDetail = resolve => {

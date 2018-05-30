782cebb2f14d4f6c8ba8df1464e2135d9e0dfd26
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-05-30 21:51:14 +0100

    [the_stock_trader_final_app] - vuex state management setup

---
+++ the_stock_trader/src/data/stocks.js
@@ -0,0 +1,6 @@
+export const stocks =  [
+	{id: 1, name: 'Apple', price: 10},
+	{id: 2, name: 'BMW', price: 12},
+	{id: 3, name: 'Audi', price: 123},
+	{id: 4, name: 'Facebook', price: 54}
+];
--- the_stock_trader/src/store/modules/stocks.js
+++ the_stock_trader/src/store/modules/stocks.js
@@ -1,7 +1,39 @@
+import {stocks} from '../../data/stocks.js';
+
 const state = {
 	stocks: [],
 };
 
 const mutations = {
+	'SET_STOCKS'(state, stocks) {
+		state.stocks = stocks;
+	},
+	'RMD_STOCK'(state) {
 
+	}
 };
+
+const actions = {
+	buyStock: ({commit}, order) => {
+		commit();
+	},
+	initStocks: ({commit}) => {
+		commit('SET_STOCKS', stocks);
+	},
+	randomizeStocks: ({commit}) => {
+		commit('RMD_STOCKS');
+	}
+};
+
+const getters = {
+	stocks: state => {
+		return state.stocks;
+	}
+};
+
+export default {
+	state,
+	mutations,
+	actions,
+	getters
+}

772cbaf503736ef51e03e9018c0b40ff5e676286
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-05-31 21:09:59 +0100

    [the_stock_trader_final_app] - adding portfolio module to vuex

---
+++ the_stock_trader/src/store/modules/portfolio.js
@@ -0,0 +1,61 @@
+const state = {
+	funds: 10000,
+	stocks: []
+};
+
+const mutations = {
+	// stockId, quantity, stockPrice - comes from order
+	'BUY_STOCK'(state, {stockId, quantity, stockPrice}) {
+		const record = state.stocks.find(element => element.id === stock.id);
+		if (record) {
+			record.quantity += quantity;
+		} else {
+			state.stocks.push({
+				id: stockId,
+				quantity: quantity
+			})
+		}
+		state.funds -= stockPrice * quantity;
+	},
+	'SELL_STOCK'(state, {stockId, quantity, stockPrice}) {
+		const record = state.stocks.find(element => element.id === stock.id);
+
+		if (record.quantity >= quantity) {
+			record.quantity -= quantity;
+		} else {
+			state.stocks.splice(state.stocks.indexOf(record), 1);
+		}
+
+		state.funds += stockPrice * quantity;
+	}
+};
+
+const actions = {
+	sellStock({commit}, order) {
+		commit('SEL_STOCK', order);
+	}
+};
+
+const getters = {
+	stockPortfolio(state, getters) {
+		return state.stocks.map(stock => {
+			const record = getters.stocks.find(element => element.id === stock.id);
+			return {
+				id: stock.id,
+				quantity: stock.quantity,
+				name: record.name,
+				price: record.price
+			}
+		})
+	},
+	funds(state) {
+		return state.funds;
+	}
+};
+
+export default {
+	state,
+	mutations,
+	actions,
+	getters
+}

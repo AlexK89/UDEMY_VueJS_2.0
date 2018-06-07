4190a4bf4357633b7b3594f12557be7f06fb0b45
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-06-07 22:13:54 +0100

    [the_stock_trader_final_app] - swich animation

--- the_stock_trader/src/App.vue
+++ the_stock_trader/src/App.vue
@@ -1,6 +1,8 @@
 <template>
 	<div class="container">
-		<router-view></router-view>
+		<transition name="slide" mode="out-in">
+			<router-view></router-view>
+		</transition>
 	</div>
 </template>
 
@@ -15,5 +17,35 @@
 <style>
 	.page {
 		padding-top: 30px;
+	}
+
+	.slide-enter-active {
+		animation: slideIn 0.3s forwards;
+	}
+
+	.slide-leave-active {
+		animation: slideOut 0.3s forwards;
+	}
+
+	@keyframes slideIn {
+		from {
+			transform: translateY(-30px);
+			opacity: 0;
+		}
+		to {
+			transform: translateY(0);
+			opacity: 1;
+		}
+	}
+
+	@keyframes slideOut {
+		from {
+			transform: translateY(0);
+			opacity: 1;
+		}
+		to {
+			transform: translateY(-30px);
+			opacity: 0;
+		}
 	}
 </style>
